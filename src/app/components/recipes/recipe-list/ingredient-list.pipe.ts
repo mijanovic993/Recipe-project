a1eda6eb2344206215784dbd2ce567739f93cbf9
stanis93 <mijanovic@outlook.com>
2021-04-12 22:16:57 +0200

    Minor code refactoring

--- src/app/components/recipes/recipe-list/ingredient-list.pipe.ts
+++ src/app/components/recipes/recipe-list/ingredient-list.pipe.ts
@@ -2,18 +2,15 @@ import { Pipe, PipeTransform } from '@angular/core';
 import { Ingredient } from 'src/app/models/ingredient.model';
 
 @Pipe({
-  name: 'ingredientList'
+  name: 'ingredientList',
 })
 export class IngredientListPipe implements PipeTransform {
   ingredientNames = [];
   transform(value: Ingredient[], ...args: unknown[]): string {
-
-    value.slice(0,3).map(
-      i => {
-        this.ingredientNames.push(i.name);
-      }
-    )
-    return value.length <= 3 ? this.ingredientNames.join(',') : this.ingredientNames.join(',') + "...";
+    value.slice(0, 3).map((i) => {
+      this.ingredientNames.push(i.name);
+    });
+    let ingredients = this.ingredientNames.join(',');
+    return value.length <= 3 ? ingredients : `${ingredients}...`;
   }
-
 }
